566b5a5a2f3af2fe9ac5e13d018e75e640628118
Vinny <vinny@Vinnys-MacBook-Pro.local>
2021-11-03 17:10:15 -0500

    changed hello world to hello Vinny

--- lib/src/main/java/comp271/assignment/one/AssignmentOne.java
+++ lib/src/main/java/comp271/assignment/one/AssignmentOne.java
@@ -2,7 +2,7 @@ package comp271.assignment.one;
 
 public class AssignmentOne {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Vinny");
 
     }
 }
